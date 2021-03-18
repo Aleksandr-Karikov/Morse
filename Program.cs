8069316d033290f7bc576b06b558492ba00cfe2a
karikov Aleksandr <kaeikov.sanya2803@outlook.com>
2021-03-18 15:04:52 +0700

    rez

--- Program.cs
+++ Program.cs
@@ -9,33 +9,36 @@ namespace morse_code
    
     class Program
     {
-        delegate string tr(string input,string language);
+        delegate string tr(string input);
         static void Main(string[] args)
         {
             tr translater;
-            string Path_read = @"C:\Users\Александр\Desktop\testread.txt";
-            string Path_write = @"C:\Users\Александр\Desktop\testwrite.txt";
-            Console.WriteLine("1 - translate from morse");
+            bool to; // перевод из или в азбукук морзе
+            string Path_read = @"C:\Users\Александр\Desktop\testread.txt"; //откуда читаем
+            string Path_write = @"C:\Users\Александр\Desktop\testwrite.txt"; //куда читаем
+            Console.WriteLine("1 - translate from morse"); //из азбуки или в азбуку
             Console.WriteLine("2 - translate to morse");
-            string key = Console.ReadLine();
+            string key = Console.ReadLine(); //в зависимости от выбора присваем делегату нужный метод и булевой переменной нужное значение
             if (key == "1")
             {
                 translater = Morse.TranslateFromMorse;
+                to = false;
             }
             else
             {
                 if (key == "2")
                 {
-                    translater = Morse.TranslateToMorse;
+                  translater = Morse.TranslateToMorse;
+                    to = true;
                 }else
                 {
                     return;
                 }
             }
-            string language="";
+            string language=""; //язык
             Console.WriteLine("1 - rus");
             Console.WriteLine("2 - eng");
-            key = Console.ReadLine();
+            key = Console.ReadLine(); //определяемся с языком
             if (key == "1")
             {
                 language = "rus";
@@ -53,15 +56,16 @@ namespace morse_code
             }
             try
             {
-                using (StreamReader sr = new StreamReader(Path_read))
+                Morse abc = new Morse(language, to); //вызываем конструктор для определения языка и превода из либо в азбуку морза
+                using (StreamReader sr = new StreamReader(Path_read)) //поток чтения из файла
                 {
-                    string line;
+                    string line; //строка для счтывания
                          while ( (line = sr.ReadLine()) != null )
                          {
                         
-                                 using (StreamWriter sw = new StreamWriter(Path_write, true, System.Text.Encoding.Default))
+                                 using (StreamWriter sw = new StreamWriter(Path_write, true, System.Text.Encoding.Default))  //поток записи
                                  {
-                                     sw.WriteLine(translater(line,language));
+                                     sw.WriteLine(translater(line)); //записываем в файл результат перевода строки
                                  }
                          }
                     Console.WriteLine("Запись завершена");
